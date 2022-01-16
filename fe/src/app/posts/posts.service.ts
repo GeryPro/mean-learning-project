1e0dfd43e166e3c7e59a6c4e3e5191b41123a75d
Gery Pro <gergana.prodanova@yahoo.com>
2022-01-16 22:09:46 +0200

    initial backend commit - node & express

--- fe/src/app/posts/posts.service.ts
+++ fe/src/app/posts/posts.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
 
@@ -10,8 +11,17 @@ export class PostsService {
 	private posts: Post[] = [];
 	private postsUpdated = new Subject<Post[]>();
 
+	constructor(private http: HttpClient) {}
+
 	getPosts() {
-		return [...this.posts];
+		this.http
+			.get<{ message: string; posts: Post[] }>(
+				'http://localhost:3000/api/posts'
+			)
+			.subscribe((postData) => {
+				this.posts = postData.posts;
+				this.postsUpdated.next([...this.posts]);
+			});
 	}
 
 	getPostsUpdatedListener() {
@@ -20,12 +30,18 @@ export class PostsService {
 
 	addPost(title: string, content: string) {
 		const post: Post = {
+			id: null,
 			title: title,
 			content: content,
 		};
 
-		this.posts.push(post);
+		this.http
+			.post<{ message: string }>('http://localhost:3000/api/posts', post)
+			.subscribe((response) => {
+				console.log(response.message);
+				this.posts.push(post);
 
-		this.postsUpdated.next([...this.posts]);
+				this.postsUpdated.next([...this.posts]);
+			});
 	}
 }
