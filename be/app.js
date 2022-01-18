0672fd5d3c59f37b23ba259ee44ee7e9c820e824
Gery Pro <gergana.prodanova@yahoo.com>
2022-01-18 21:56:29 +0200

    add mongodb

--- be/app.js
+++ be/app.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const Post = require('./models/post');
 
 const app = express();
 app.use(express.json());
+
+mongoose.connect('mongodb://127.0.0.1:27017/mean-prj');
 
 app.use((req, res, next) => {
 	res.setHeader('Access-Control-Allow-Origin', '*');
@@ -17,27 +21,21 @@ app.use((req, res, next) => {
 });
 
 app.get('/api/posts', (req, res) => {
-	const posts = [
-		{
-			id: 'ADassfasfe12',
-			title: 'First server-side post',
-			content: 'This is coming from the server',
-		},
-		{
-			id: 'qwerty1341qw',
-			title: 'Second server-side post',
-			content: 'This is coming from the server!',
-		},
-	];
-	res.status(200).json({
-		message: 'Posts fetched successfully!',
-		posts: posts,
-	});
+	Post.find().then((documents) =>
+		res.status(200).json({
+			message: 'Posts fetched successfully!',
+			posts: documents,
+		})
+	);
 });
 
 app.post('/api/posts', (req, res) => {
-	const post = req.body;
-	console.log(post);
+	const post = new Post({
+		title: req.body.title,
+		content: req.body.content,
+	});
+
+	post.save();
 	res.status(201).json({ message: 'Post added successfully.' });
 });
 
--- fe/src/app/posts/posts.service.ts
+++ fe/src/app/posts/posts.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Subject } from 'rxjs';
+import { Observable, Subject } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 import { Post } from './post.model';
 
@@ -13,22 +14,27 @@ export class PostsService {
 
 	constructor(private http: HttpClient) {}
 
-	getPosts() {
+	getPosts(): void {
 		this.http
-			.get<{ message: string; posts: Post[] }>(
-				'http://localhost:3000/api/posts'
+			.get<{ message: string; posts: any }>('http://localhost:3000/api/posts')
+			.pipe(
+				map((postData) => {
+					return postData.posts.map((post: any) => {
+						return { title: post.title, content: post.content, id: post._id };
+					});
+				})
 			)
-			.subscribe((postData) => {
-				this.posts = postData.posts;
+			.subscribe((transformedPosts) => {
+				this.posts = transformedPosts;
 				this.postsUpdated.next([...this.posts]);
 			});
 	}
 
-	getPostsUpdatedListener() {
+	getPostsUpdatedListener(): Observable<Post[]> {
 		return this.postsUpdated.asObservable();
 	}
 
-	addPost(title: string, content: string) {
+	addPost(title: string, content: string): void {
 		const post: Post = {
 			id: null,
 			title: title,
